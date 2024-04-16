8a637e191f7a481ce32f1f94bb282ad95c92929d
1 <you@example.com>
2024-04-16 14:29:31 +0800

    [feat]: init

---
+++ src/main/java/com/battlebyte/battlebyte/BattleByteApplication.java
@@ -0,0 +1,13 @@
+package com.battlebyte.battlebyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleByteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleByteApplication.class, args);
+	}
+
+}
