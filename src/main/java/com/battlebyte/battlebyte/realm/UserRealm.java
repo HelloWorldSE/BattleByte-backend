671a76288a269f98f60be86dcfd7d8225af1d6ba
1 <you@example.com>
2024-04-20 19:25:51 +0800

    [feat]: 权限功能有问题，先去掉

--- src/main/java/com/battlebyte/battlebyte/realm/UserRealm.java
+++ src/main/java/com/battlebyte/battlebyte/realm/UserRealm.java
@@ -1,47 +1,47 @@
-package com.battlebyte.battlebyte.realm;
-
-import com.battlebyte.battlebyte.entity.User;
-import com.battlebyte.battlebyte.service.UserService;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.*;
-import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.HashSet;
-
-@Component
-public class UserRealm extends AuthorizingRealm {
-    @Autowired
-    private UserService userService;
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-
-        Subject subject = SecurityUtils.getSubject();
-        User user = (User) subject.getPrincipal();
-
-        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
-
-
-        HashSet<String> roles = new HashSet<>(userService.getRole(1));
-        HashSet<String> permissions = new HashSet<>(userService.getPermission(1));
-
-        simpleAuthorizationInfo.setRoles(roles);
-        simpleAuthorizationInfo.setStringPermissions(permissions);
-        return simpleAuthorizationInfo;
-    }
-
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException { // 认证：用户的权限信息
-        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
-        User user = userService.findByUserName(token.getUsername());
-        if (user != null) {
-            return new SimpleAuthenticationInfo(user, user.getPassword(), getName());
-        }
-        return null;
-    }
-}
+//package com.battlebyte.battlebyte.realm;
+//
+//import com.battlebyte.battlebyte.entity.User;
+//import com.battlebyte.battlebyte.service.UserService;
+//import org.apache.shiro.SecurityUtils;
+//import org.apache.shiro.authc.*;
+//import org.apache.shiro.authz.AuthorizationInfo;
+//import org.apache.shiro.authz.SimpleAuthorizationInfo;
+//import org.apache.shiro.realm.AuthorizingRealm;
+//import org.apache.shiro.subject.PrincipalCollection;
+//import org.apache.shiro.subject.Subject;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.HashSet;
+//
+//@Component
+//public class UserRealm extends AuthorizingRealm {
+//    @Autowired
+//    private UserService userService;
+//    @Override
+//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+//
+//        Subject subject = SecurityUtils.getSubject();
+//        User user = (User) subject.getPrincipal();
+//
+//        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+//
+//
+//        HashSet<String> roles = new HashSet<>(userService.getRole(1));
+//        HashSet<String> permissions = new HashSet<>(userService.getPermission(1));
+//
+//        simpleAuthorizationInfo.setRoles(roles);
+//        simpleAuthorizationInfo.setStringPermissions(permissions);
+//        return simpleAuthorizationInfo;
+//    }
+//
+//    @Override
+//    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException { // 认证：用户的权限信息
+//        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+//        User user = userService.findByUserName(token.getUsername());
+//        if (user != null) {
+//            return new SimpleAuthenticationInfo(user, user.getPassword(), getName());
+//        }
+//        return null;
+//    }
+//}
