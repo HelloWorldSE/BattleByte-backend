5d7010b9aaba84210c49ba8cb1dedc66cf7758c6
1 <you@example.com>
2024-04-26 22:10:24 +0800

    [feat]: 一些基本的功能

--- src/main/java/com/battlebyte/battlebyte/controller/GameController.java
+++ src/main/java/com/battlebyte/battlebyte/controller/GameController.java
@@ -1,9 +1,15 @@
 package com.battlebyte.battlebyte.controller;
 
 import com.battlebyte.battlebyte.entity.Game;
+import com.battlebyte.battlebyte.entity.dto.UserInfoDTO;
 import com.battlebyte.battlebyte.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/game")
 @RestController
@@ -26,6 +32,10 @@ public class GameController {
         gameService.updateGame(game);
     }
 
-
+    @GetMapping("/player")
+    public Page<UserInfoDTO> getPlayer(@RequestParam Integer id, @RequestParam(defaultValue = "1") Integer page,
+                                       @RequestParam(defaultValue = "10") Integer pageSize) {
+        return gameService.getPlayer(id, PageRequest.of(page - 1, pageSize));
+    }
 
 }
