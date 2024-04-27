26c2a3b6410600c4eedc3693430dbf9dc45b315f
Jefferrrrrrrrrry <2358931408@qq.com>
2024-04-27 21:53:01 +0800

    [fix]修复oj接口

--- src/main/java/com/battlebyte/battlebyte/service/OJService.java
+++ src/main/java/com/battlebyte/battlebyte/service/OJService.java
@@ -13,7 +13,7 @@ public class OJService {
             "csrftoken=2YPsM0GU3sS9jsSAOSCFMuHqKNqblm0Ce6pjtZeErHtceTBXFS1poqm3fCPNNQMI; " +
             "sessionid=fmlxdqc9dj0abgqh8ycgq2cxz8hi2axl";
     public String X_Csrftoken="2YPsM0GU3sS9jsSAOSCFMuHqKNqblm0Ce6pjtZeErHtceTBXFS1poqm3fCPNNQMI";
-    public String getProblem(Integer id) {
+    public JSONObject getProblem(Integer id) {
         String url = "http://81.70.241.166:1233/api/problem?problem_id=" + id;
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Csrftoken", X_Csrftoken);
@@ -25,7 +25,7 @@ public class OJService {
         return getString(url, requestEntity);
     }
 
-    public String submit(String input)  {
+    public JSONObject submit(String input)  {
         String url = "http://81.70.241.166:1233/api/submission";
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Csrftoken", X_Csrftoken);
@@ -34,15 +34,13 @@ public class OJService {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<String> requestEntity = new HttpEntity<>(input.replace(' ', ' '), headers);
-        String result =  postString(url, requestEntity);
-
-        JSONObject resultObj = JSON.parseObject(result);
-        JSONObject data = resultObj.getJSONObject("data");
+        JSONObject result =  postString(url, requestEntity);
+        JSONObject data = result.getJSONObject("data");
         String submission_id = data.getString("submission_id");
         return getResult(submission_id);
     }
 
-    public String getResult(String input) {
+    public JSONObject getResult(String input) {
         String url = "http://81.70.241.166:1233/api/submission?id=" + input;
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Csrftoken", X_Csrftoken);
@@ -52,9 +50,9 @@ public class OJService {
         return getString(url, requestEntity);
     }
 
-    private static String getString(String url, HttpEntity<String> requestEntity) {
+    private static JSONObject getString(String url, HttpEntity<String> requestEntity) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<JSONObject> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, JSONObject.class);
         HttpStatusCode statusCode = responseEntity.getStatusCode();
         if (statusCode == HttpStatus.OK) {
             return responseEntity.getBody();
@@ -62,9 +60,9 @@ public class OJService {
             throw new ServiceException("访问失败");
         }
     }
-    private static String postString(String url, HttpEntity<String> requestEntity) {
+    private static JSONObject postString(String url, HttpEntity<String> requestEntity) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<JSONObject> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, JSONObject.class);
 
         HttpStatusCode statusCode = responseEntity.getStatusCode();
         if (statusCode == HttpStatus.OK) {
