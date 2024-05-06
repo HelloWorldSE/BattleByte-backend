4791344e05fb2790c3eaef9469ac6994fdc84983
1 <you@example.com>
2024-05-06 23:27:32 +0800

    [fix]: 好友接口

--- src/main/java/com/battlebyte/battlebyte/entity/dto/FriendDTO.java
+++ src/main/java/com/battlebyte/battlebyte/entity/dto/FriendDTO.java
@@ -1,2 +1,5 @@
-package com.battlebyte.battlebyte.entity.dto;public interface FriendDTO {
+package com.battlebyte.battlebyte.entity.dto;
+
+public interface FriendDTO extends UserInfoDTO {
+    Integer getFriendId();
 }
