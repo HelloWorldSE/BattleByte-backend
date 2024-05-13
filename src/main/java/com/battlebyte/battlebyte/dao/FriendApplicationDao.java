b4ebe97f4b6b0836f422bc9a7fdaf91db549ce2f
1 <you@example.com>
2024-05-13 16:08:14 +0800

    [fix]: 好友申请查询有误

--- src/main/java/com/battlebyte/battlebyte/dao/FriendApplicationDao.java
+++ src/main/java/com/battlebyte/battlebyte/dao/FriendApplicationDao.java
@@ -20,9 +20,7 @@ public interface FriendApplicationDao extends JpaRepository<FriendApplication, I
             "FROM \n" +
             "    user u\n" +
             "JOIN \n" +
-            "    friend_application fa ON u.id = fa.sender_id\n" +
-            "JOIN \n" +
-            "    user f ON fa.receiver_id = f.id\n" +
+            "    friend_application f ON u.id = f.sender_id\n" +
             "WHERE \n" +
             "    u.id = CASE WHEN ?1 != 0 THEN ?1 ELSE u.id END\n" +
             "    AND u.user_name LIKE CONCAT('%', ?2, '%')\n" +
