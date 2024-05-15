fc4f0ac262cca2e414f0b41d72075c267a7cab86
Jefferrrrrrrrrry <2358931408@qq.com>
2024-05-15 22:36:29 +0800

    [feat]增加bot

---
+++ src/main/java/com/battlebyte/battlebyte/util/FakePlayer.java
@@ -0,0 +1,45 @@
+package com.battlebyte.battlebyte.util;
+
+import com.battlebyte.battlebyte.websocket.CurrentGame;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FakePlayer extends Thread{
+    private final CurrentGame game;
+    private ReentrantLock lock = new ReentrantLock();
+    public FakePlayer(CurrentGame game){
+        this.game=game;
+    }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                lock.lock();
+                LocalDateTime start=game.getCurrentTime();
+                LocalDateTime end=LocalDateTime.now();
+                Duration duration = Duration.between(start, end);
+                long seconds = duration.getSeconds();
+                System.out.println("时间差：" + seconds + "秒");
+                if (seconds>180) {
+                    //TODO
+                    lock.unlock();
+                    break;
+                }else {
+                    if(seconds%5==0){
+                        //TODO
+                    }
+                    lock.unlock();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+
+}
--- src/main/java/com/battlebyte/battlebyte/websocket/CurrentGame.java
+++ src/main/java/com/battlebyte/battlebyte/websocket/CurrentGame.java
@@ -3,10 +3,11 @@ package com.battlebyte.battlebyte.websocket;
 import lombok.Data;
 
 import java.util.Map;
-
+import java.time.LocalDateTime;
 @Data
 public class CurrentGame {
     int gameId;
     int questionId;
+    LocalDateTime currentTime;
     Map<String, Integer> playerMap;
 }
