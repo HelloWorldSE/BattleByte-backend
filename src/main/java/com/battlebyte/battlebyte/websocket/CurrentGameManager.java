1f62852a7ebcc2d48a4c397166b31fd601a1ac83
hiGiraffe <chenjulian.buaa@gmail.com>
2024-05-30 00:29:57 +0800

    [fix]: 复原

--- src/main/java/com/battlebyte/battlebyte/websocket/CurrentGameManager.java
+++ src/main/java/com/battlebyte/battlebyte/websocket/CurrentGameManager.java
@@ -1,6 +1,5 @@
 package com.battlebyte.battlebyte.websocket;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +7,7 @@ import java.io.IOException;
 
 @Component
 public class CurrentGameManager {
-    private WebSocketServer webSocketServer;
-    @Autowired
-    public CurrentGameManager(WebSocketServer webSocketServer){
-        this.webSocketServer=webSocketServer;
-    }
+    WebSocketServer webSocketServer = new WebSocketServer();
     @Scheduled(fixedRate = 2000)
     public void performTask() throws IOException {
         webSocketServer.manageGame();
