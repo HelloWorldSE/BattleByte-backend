12a2ad7e444e65bddc45821bafbc4d0b2fe5c979
adambwcao <adambwcao@sina.com>
2024-06-04 16:18:07 +0800

    [feat]聊天道具和房间websocket单元测试

--- src/main/java/com/battlebyte/battlebyte/websocket/MyWebSocketClient.java
+++ src/main/java/com/battlebyte/battlebyte/websocket/MyWebSocketClient.java
@@ -9,27 +9,30 @@ import java.net.URI;
 
 @Slf4j
 public class MyWebSocketClient extends WebSocketClient {
-    public MyWebSocketClient(URI uri) {
+    private int uid;
+    
+    public MyWebSocketClient(URI uri, int uid) {
         super(uri);
+        this.uid = uid;
     }
     
     @Override
     public void onOpen(ServerHandshake serverHandshake) {
-        log.info("客户端连接成功");
+        log.info("客户端" + uid + "连接成功");
     }
     
     @Override
     public void onMessage(String message) {
-        log.info("客户端接收到消息：" + message);
+        log.info("客户端" + uid + "接收到消息：" + message);
     }
     
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        log.info("客户端关闭成功");
+        log.info("客户端" + uid + "关闭成功");
     }
     
     @Override
     public void onError(Exception ex) {
-        log.error("客户端出错", ex);
+        log.error("客户端" + uid + "出错", ex);
     }
 }
