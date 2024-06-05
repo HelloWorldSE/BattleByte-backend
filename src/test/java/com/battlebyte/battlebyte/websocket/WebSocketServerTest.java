df30c4a4bf8e4753757778345747d6886253e969
adambwcao <adambwcao@sina.com>
2024-06-05 22:57:37 +0800

    [feat]进出房间获取房间信息websocket单元测试

--- src/test/java/com/battlebyte/battlebyte/websocket/WebSocketServerTest.java
+++ src/test/java/com/battlebyte/battlebyte/websocket/WebSocketServerTest.java
@@ -2,6 +2,7 @@ package com.battlebyte.battlebyte.websocket;
 
 import com.alibaba.fastjson.JSONObject;
 import com.battlebyte.battlebyte.service.MatchService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.WebSocket;
 import org.junit.Before;
@@ -143,37 +144,37 @@ public class WebSocketServerTest {
             log.info("WebSocket客户端连接中，请稍等...");
             Thread.sleep(500);
         }
-    
+        
         JSONObject output_LOGIN_REQ = new JSONObject();
         JSONObject dataOutput_LOGIN_REQ = new JSONObject();
         dataOutput_LOGIN_REQ.put("token", 1);
         output_LOGIN_REQ.put("type", "LOGIN_REQ");
         output_LOGIN_REQ.put("data", dataOutput_LOGIN_REQ);
         output_LOGIN_REQ.put("id", "1");
-    
+        
         JSONObject output_LOGIN_REQ2 = new JSONObject();
         JSONObject dataOutput_LOGIN_REQ2 = new JSONObject();
         dataOutput_LOGIN_REQ2.put("token", 2);
         output_LOGIN_REQ2.put("type", "LOGIN_REQ");
         output_LOGIN_REQ2.put("data", dataOutput_LOGIN_REQ2);
         output_LOGIN_REQ2.put("id", "1");
-    
+        
         JSONObject output_MATCH_REQ = new JSONObject();
         JSONObject dataOutput_MATCH_REQ = new JSONObject();
         dataOutput_MATCH_REQ.put("type", 1);
         output_MATCH_REQ.put("type", "MATCH_REQ");
         output_MATCH_REQ.put("data", dataOutput_MATCH_REQ);
         output_MATCH_REQ.put("id", "1");
-    
+        
         myWebSocketClient1.send(output_LOGIN_REQ.toJSONString());
         Thread.sleep(500); // 需要等待server端给client发回信息，不能让client提前结束
         myWebSocketClient1.send(output_MATCH_REQ.toJSONString());
-    
+        
         myWebSocketClient2.send(output_LOGIN_REQ2.toJSONString());
         Thread.sleep(500);
         myWebSocketClient2.send(output_MATCH_REQ.toJSONString());
         Thread.sleep(3000); // 等待MATCH_START
-    
+        
         JSONObject output_ITEM_SEND = new JSONObject();
         JSONObject dataOutput_ITEM_SEND = new JSONObject();
         dataOutput_ITEM_SEND.put("type", "tomato");
@@ -181,7 +182,7 @@ public class WebSocketServerTest {
         output_ITEM_SEND.put("data", dataOutput_ITEM_SEND);
         output_ITEM_SEND.put("id", "1");
         myWebSocketClient1.send(output_ITEM_SEND.toJSONString());
-    
+        
         JSONObject output_ITEM_SEND2 = new JSONObject();
         JSONObject dataOutput_ITEM_SEND2 = new JSONObject();
         dataOutput_ITEM_SEND2.put("type", "tomato3");
@@ -190,7 +191,7 @@ public class WebSocketServerTest {
         output_ITEM_SEND2.put("id", "1");
         myWebSocketClient2.send(output_ITEM_SEND2.toJSONString());
         Thread.sleep(1000);
-    
+        
         JSONObject output_SURRENDER = new JSONObject();
         JSONObject dataOutput_SURRENDER = new JSONObject();
         output_SURRENDER.put("type", "SURRENDER");
@@ -200,36 +201,37 @@ public class WebSocketServerTest {
         Thread.sleep(5000);
     }
     
+    @Transactional
     @Test
-    @Rollback
+    @Rollback()
     public void roomTest() throws Exception { // 经测试，room_request的out无法移除房间内用户
         MyWebSocketClient myWebSocketClient2 = new MyWebSocketClient(new URI("ws://localhost:9090/server"), 2); // client能和server通信，主要通过这个url，能找到彼此
         myWebSocketClient2.connect();
-
+        
         MyWebSocketClient myWebSocketClient1 = new MyWebSocketClient(new URI("ws://localhost:9090/server"), 5); // client能和server通信，主要通过这个url，能找到彼此
         myWebSocketClient1.connect();
         while (!myWebSocketClient1.getReadyState().equals(ReadyState.OPEN)) {
             log.info("WebSocket客户端连接中，请稍等...");
             Thread.sleep(500);
         }
-    
+        
         JSONObject output_LOGIN_REQ = new JSONObject();
         JSONObject dataOutput_LOGIN_REQ = new JSONObject();
         dataOutput_LOGIN_REQ.put("token", 1);
         output_LOGIN_REQ.put("type", "LOGIN_REQ");
         output_LOGIN_REQ.put("data", dataOutput_LOGIN_REQ);
         output_LOGIN_REQ.put("id", "1");
-    
+        
         myWebSocketClient1.send(output_LOGIN_REQ.toJSONString());
         Thread.sleep(500); // 需要等待server端给client发回信息，不能让client提前结束
-
+        
         JSONObject output_LOGIN_REQ2 = new JSONObject();
         JSONObject dataOutput_LOGIN_REQ2 = new JSONObject();
-        dataOutput_LOGIN_REQ2.put("token", 2);
+        dataOutput_LOGIN_REQ2.put("token", 2);  // 2号id在之前的测试中已存在多个房间内，包括本函数测试的15号房间，所以不需要发room_request也在房间内
         output_LOGIN_REQ2.put("type", "LOGIN_REQ");
         output_LOGIN_REQ2.put("data", dataOutput_LOGIN_REQ2);
         output_LOGIN_REQ2.put("id", "1");
-
+        
         myWebSocketClient2.send(output_LOGIN_REQ2.toJSONString());
         Thread.sleep(500); // 需要等待server端给client发回信息，不能让client提前结束
         
@@ -242,7 +244,7 @@ public class WebSocketServerTest {
         output_ROOM_REQUEST.put("id", "1");
         myWebSocketClient1.send(output_ROOM_REQUEST.toJSONString());
         Thread.sleep(500);
-    
+        
         JSONObject output_ROOM_REQUEST2 = new JSONObject();
         JSONObject dataOutput_ROOM_REQUEST2 = new JSONObject();
         dataOutput_ROOM_REQUEST2.put("roomid", 15);
@@ -256,8 +258,9 @@ public class WebSocketServerTest {
         Thread.sleep(5000);
     }
     
+    @Transactional
     @Test
-    @Rollback
+    @Rollback()
     public void roomStartTest() throws InterruptedException, URISyntaxException {
         // 经测试,room_request类型请求你不能多个客户端同时send，否则只能收到1条room_refresh消息，而不是每个客户端收到一条
         MyWebSocketClient myWebSocketClient1 = new MyWebSocketClient(new URI("ws://localhost:9090/server"), 5); // client能和server通信，主要通过这个url，能找到彼此
@@ -280,17 +283,104 @@ public class WebSocketServerTest {
             log.info("WebSocket客户端连接中，请稍等...");
             Thread.sleep(500);
         }
-    
+        
         JSONObject output_LOGIN_REQ = new JSONObject();
         JSONObject dataOutput_LOGIN_REQ = new JSONObject();
         dataOutput_LOGIN_REQ.put("token", 1);
         output_LOGIN_REQ.put("type", "LOGIN_REQ");
         output_LOGIN_REQ.put("data", dataOutput_LOGIN_REQ);
         output_LOGIN_REQ.put("id", "1");
-    
+        
+        JSONObject output_LOGIN_REQ2 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ2 = new JSONObject();
+        dataOutput_LOGIN_REQ2.put("token", 2);
+        output_LOGIN_REQ2.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ2.put("data", dataOutput_LOGIN_REQ2);
+        output_LOGIN_REQ2.put("id", "1");
+        
+        JSONObject output_LOGIN_REQ3 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ3 = new JSONObject();
+        dataOutput_LOGIN_REQ3.put("token", 3);
+        output_LOGIN_REQ3.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ3.put("data", dataOutput_LOGIN_REQ3);
+        output_LOGIN_REQ3.put("id", "1");
+        
+        JSONObject output_LOGIN_REQ4 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ4 = new JSONObject();
+        dataOutput_LOGIN_REQ4.put("token", 4);
+        output_LOGIN_REQ4.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ4.put("data", dataOutput_LOGIN_REQ4);
+        output_LOGIN_REQ4.put("id", "1");
+        
+        JSONObject output_LOGIN_REQ5 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ5 = new JSONObject();
+        dataOutput_LOGIN_REQ5.put("token", 5);
+        output_LOGIN_REQ5.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ5.put("data", dataOutput_LOGIN_REQ5);
+        output_LOGIN_REQ5.put("id", "1");
+        
+        JSONObject output_LOGIN_REQ6 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ6 = new JSONObject();
+        dataOutput_LOGIN_REQ6.put("token", 6);
+        output_LOGIN_REQ6.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ6.put("data", dataOutput_LOGIN_REQ6);
+        output_LOGIN_REQ6.put("id", "1");
+        
+        JSONObject output_LOGIN_REQ7 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ7 = new JSONObject();
+        dataOutput_LOGIN_REQ7.put("token", 7);
+        output_LOGIN_REQ7.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ7.put("data", dataOutput_LOGIN_REQ7);
+        output_LOGIN_REQ7.put("id", "1");
+        
+        JSONObject output_LOGIN_REQ8 = new JSONObject();
+        JSONObject dataOutput_LOGIN_REQ8 = new JSONObject();
+        dataOutput_LOGIN_REQ8.put("token", 8);
+        output_LOGIN_REQ8.put("type", "LOGIN_REQ");
+        output_LOGIN_REQ8.put("data", dataOutput_LOGIN_REQ8);
+        output_LOGIN_REQ8.put("id", "1");
+        
         myWebSocketClient1.send(output_LOGIN_REQ.toJSONString());
         Thread.sleep(500); // 需要等待server端给client发回信息，不能让client提前结束
-    
+        myWebSocketClient2.send(output_LOGIN_REQ2.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient3.send(output_LOGIN_REQ3.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient4.send(output_LOGIN_REQ4.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient5.send(output_LOGIN_REQ5.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient6.send(output_LOGIN_REQ6.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient7.send(output_LOGIN_REQ7.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient8.send(output_LOGIN_REQ8.toJSONString());
+        Thread.sleep(500);
+        
+        JSONObject output_ROOM_REQUEST_OUT = new JSONObject();
+        JSONObject dataOutput_ROOM_REQUEST_OUT = new JSONObject();
+        dataOutput_ROOM_REQUEST_OUT.put("roomid", 30);
+        dataOutput_ROOM_REQUEST_OUT.put("type", "out");
+        output_ROOM_REQUEST_OUT.put("type", "ROOM_REQUEST");
+        output_ROOM_REQUEST_OUT.put("data", dataOutput_ROOM_REQUEST_OUT);
+        output_ROOM_REQUEST_OUT.put("id", "1");
+        myWebSocketClient1.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient2.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient3.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient4.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient5.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient6.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient7.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        myWebSocketClient8.send(output_ROOM_REQUEST_OUT.toJSONString());
+        Thread.sleep(500);
+        
         JSONObject output_ROOM_REQUEST = new JSONObject();
         JSONObject dataOutput_ROOM_REQUEST = new JSONObject();
         dataOutput_ROOM_REQUEST.put("roomid", 30);
@@ -314,13 +404,23 @@ public class WebSocketServerTest {
         Thread.sleep(500);
         myWebSocketClient8.send(output_ROOM_REQUEST.toJSONString());
         Thread.sleep(500);
-    
+        
+        JSONObject output_ROOM_GET_INFO = new JSONObject();
+        JSONObject dataOutput_ROOM_GET_INFO = new JSONObject();
+        dataOutput_ROOM_GET_INFO.put("roomid", 30);
+        output_ROOM_GET_INFO.put("type", "ROOM_GET_INFO");
+        output_ROOM_GET_INFO.put("data", dataOutput_ROOM_GET_INFO);
+        output_ROOM_GET_INFO.put("id", "1");
+        myWebSocketClient1.send(output_ROOM_GET_INFO.toJSONString());
+        Thread.sleep(1000);
+        
         JSONObject output_ROOM_START = new JSONObject();
         JSONObject dataOutput_ROOM_START = new JSONObject();
         dataOutput_ROOM_START.put("roomid", 30);
-        output_ROOM_START.put("type", "ROOM_START");
-        output_ROOM_START.put("data", dataOutput_ROOM_REQUEST);
+        output_ROOM_START.put("type", "ROOM_START"); // 暂时有问题，无法测试
+        output_ROOM_START.put("data", dataOutput_ROOM_START);
         output_ROOM_START.put("id", "1");
+        myWebSocketClient1.send(output_ROOM_START.toJSONString());
         Thread.sleep(5000);
     }
 }
